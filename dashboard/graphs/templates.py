da86c449b8b346f4fc2d3c33762e7a46f2e2ad21
paliwko <74672529+paliwko@users.noreply.github.com>
2022-06-18 15:27:41 +0200

    Update templates.py

--- dashboard/graphs/templates.py
+++ dashboard/graphs/templates.py
@@ -3,22 +3,19 @@ from typing import List
 import plotly.express as px
 import plotly.graph_objects as go
 from dash import dcc
-from data.external import set_df_f
+from data.external import filter_data, set_df_f
 from plotly.subplots import make_subplots
 
 
 def scatter_top(amp_range_f: List[float] = None, seq_y_vals: List[int] = None):
     _df = set_df_f()
-    if amp_range_f is not None:
-        _df = _df.loc[_df.dda50Amplitude.between(*amp_range_f)]
-    if seq_y_vals is not None:
-        _df = _df.loc[_df.Testpointsequencenumber_y.isin(seq_y_vals)]
+    _df = filter_data(_df, amp_range_f, seq_y_vals)
 
     scatter_fig = px.scatter(
         _df,
         x="r12_load_MW",
         y="dda50FrequencyHz",
-        title="test",
+        title="Sensor response vs Frequency&Load",
         # colorscale="Inferno",
         # showscale=True
         hover_data=[
@@ -39,10 +36,7 @@ def scatter_top(amp_range_f: List[float] = None, seq_y_vals: List[int] = None):
 
 def scatter_1(amp_range_f: List[float] = None, seq_y_vals: List[int] = None):
     _df = set_df_f()
-    if amp_range_f is not None:
-        _df = _df.loc[_df.dda50Amplitude.between(*amp_range_f)]
-    if seq_y_vals is not None:
-        _df = _df.loc[_df.Testpointsequencenumber_y.isin(seq_y_vals)]
+    _df = filter_data(_df, amp_range_f, seq_y_vals)
     # Create figure with secondary y-axis
 
     fig = make_subplots(specs=[[{"secondary_y": True}]])
@@ -59,7 +53,7 @@ def scatter_1(amp_range_f: List[float] = None, seq_y_vals: List[int] = None):
         secondary_y=False,
     )
 
-    fig.update_layout(title_text="Dual axis scatter")
+    fig.update_layout(title_text="Load / Speed / Amplitude in time")
     fig.update_xaxes(title_text="Time")
     fig.update_yaxes(title_text="Speed", secondary_y=False)
     fig.update_yaxes(title_text="Amplitude", secondary_y=True)
@@ -67,19 +61,12 @@ def scatter_1(amp_range_f: List[float] = None, seq_y_vals: List[int] = None):
     fig.update_layout(height=700)
     fig.update_layout(legend=dict(yanchor="top", y=0.99, xanchor="right", x=0.99))
 
-    return dcc.Graph(
-        id="scatter-1",
-        figure=fig
-
-    )
+    return dcc.Graph(id="scatter-1", figure=fig)
 
 
 def scatter_2(amp_range_f: List[float] = None, seq_y_vals: List[int] = None):
     _df = set_df_f()
-    if amp_range_f is not None:
-        _df = _df.loc[_df.dda50Amplitude.between(*amp_range_f)]
-    if seq_y_vals is not None:
-        _df = _df.loc[_df.Testpointsequencenumber_y.isin(seq_y_vals)]
+    _df = filter_data(_df, amp_range_f, seq_y_vals)
     # Create figure with secondary y-axis
 
     fig = make_subplots(specs=[[{"secondary_y": True}]])
@@ -98,5 +85,3 @@ def scatter_2(amp_range_f: List[float] = None, seq_y_vals: List[int] = None):
     fig.update_traces(dict(mode="markers"))
 
     return dcc.Graph(id="scatter-2", figure=fig)
-
-
