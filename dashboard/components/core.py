62d2d74dcaf8a826581bb1a7ea23cf392db63378
paliwko <74672529+paliwko@users.noreply.github.com>
2022-06-18 03:01:12 +0200

    Add files via upload
    nie wiem jak dodac commity do innego brancha niz master, ciagle "nothing to pull"... :/

---
+++ dashboard/components/core.py
@@ -0,0 +1,22 @@
+import dash_bootstrap_components as dbc
+from dash import html
+
+test_logo = "https://cdn-icons-png.flaticon.com/512/1875/1875654.png"
+header = dbc.Navbar(
+    dbc.Container(
+        [
+            dbc.Row(
+                [
+                    dbc.Col(html.Img(src=test_logo, height="100"), align="left"),
+                    dbc.Col(
+                        dbc.NavbarBrand("Turbine Test Data Reviewer", className="ms-2")
+                    ),
+                ],
+                align="center",
+                className="g-0",
+            ),
+        ]
+    ),
+    color="light",
+    dark=False,
+)
